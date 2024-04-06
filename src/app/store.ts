90630bd2b296c3fd5ffaf10e8722cd80e3ec200d
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2024-04-06 11:22:29 +0300

    Sprint 5, lesson 19 - 2

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import { tasksReducer } from "features/TodolistsList/tasks.reducer";
+import { todolistsReducer } from "features/TodolistsList/todolists.reducer";
+import { appReducer } from "app/app.reducer";
+import { authReducer } from "features/auth/auth.reducer";
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export const store = configureStore({
+  reducer: {
+      tasks: tasksReducer,
+      todolists: todolistsReducer,
+      app: appReducer,
+      auth: authReducer,
+  }
+});
+
+export type AppRootStateType = ReturnType<typeof store.getState>;
+
+
+export type AppDispatch = typeof store.dispatch;
---
+++ src/setupTests.ts
@@ -0,0 +1,6 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+// import "@testing-library/jest-dom/extend-expect"; Я закомментировал и тесты стали проходить, хотя это был "заводской"
+// код
