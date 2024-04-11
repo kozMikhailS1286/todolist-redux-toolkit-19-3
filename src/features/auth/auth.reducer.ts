65471bfff8044fceebba9d501e296373abffad6e
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2024-04-11 12:16:32 +0300

    Sprint 5, lesson 19 - 8-9

--- src/features/auth/auth.reducer.ts
+++ src/features/auth/auth.reducer.ts
@@ -40,7 +40,9 @@ const login = createAppAsyncThunk<{ isLoggedIn: boolean }, LoginParamsType>(
         dispatch(appActions.setAppStatus({ status: "succeeded" }));
         return { isLoggedIn: true };
       } else {
-        handleServerAppError(res.data, dispatch, false);
+          debugger
+        const isShowAppError = !res.data.fieldsErrors.length
+        handleServerAppError(res.data, dispatch, isShowAppError);
         return rejectWithValue(res.data);
       }
     } catch (e) {
